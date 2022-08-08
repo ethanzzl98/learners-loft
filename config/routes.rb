928b30c49cced55e502b1fd7577c8039a75a4506
Ethan Teh <tehzl@hotmail.com>
2022-08-08 16:56:31 +0800

    add an action

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :lessons, only: [:index, :create, :show, :update, :destroy] do
-        resources :bookings, only: [:create, :update]
+        resources :bookings, only: [:create, :show, :update]
       end
     end
   end

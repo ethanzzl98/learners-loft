60db01c5794acc80034bb730858970b048bea685
Ethan Teh <tehzl@hotmail.com>
2022-08-09 15:26:44 +0800

    database1

--- db/migrate/20220808065740_create_users.rb
+++ db/migrate/20220808065740_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :open_id
+      t.string :full_name
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.create([{ first_name: "Jerry", last_name: "Tong", bio: "Best History Teacher in Shanghai" },
-{ first_name: "Ethan", last_name: "Zhang", bio: "Best Back End programmer in history" },
-{ first_name: "Stuart", last_name: "Schuman", bio: "Best Academic English Teacher in Shanghai" },
-{ first_name: "Illarion", last_name: "Kopeika", bio: "Best History Teacher in Shanghai" }])
-
 lesson = Lesson.create([{ subject: "History", title: "1942", description: "Best History class in Shanghai", start_time: DateTime.new(2022,8,30,8), end_time: DateTime.new(2022,8,30,9), user_id: 1},
 { subject: "English", title: "English present simple tense", description: "Best Back End class in history", start_time: DateTime.new(2022,9,30,8), end_time: DateTime.new(2022,9,30,9), user_id: 2},
 { subject: "Science", title: "Physics", description: "Best Academic English class in Shanghai", start_time: DateTime.new(2021,3,30,8), end_time: DateTime.new(2021,3,30,9), user_id: 3},

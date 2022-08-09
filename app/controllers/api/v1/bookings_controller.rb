48daa193e1c922c7752179ca8fa62351be180e45
Jerry Tong <jerrytong1@gmail.com>
2022-08-09 10:39:43 +0800

    updated controllers

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -22,4 +22,9 @@ class Api::V1::BookingsController < Api::V1::BaseController
   def booking_params
     params.require(:booking).permit(:class_id, :user_id, :status)
   end
+
+  def render_error
+    render json: { errors: @booking.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end

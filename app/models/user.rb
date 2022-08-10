a5d19394968e8d047d3093dc5c688c425799ea66
ethan39 <105428625+ethanzzl98@users.noreply.github.com>
2022-08-10 14:30:14 +0800

    We don't need that

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :lessons
   has_many :bookings
-  has_many :booked_lessons, through: :bookings, source: :lesson
+  #has_many :booked_lessons, through: :bookings, source: :lesson
 end

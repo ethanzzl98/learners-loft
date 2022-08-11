bba34669c30b678296873a0964a48f43e44974a3
Jerry Tong <jerrytong1@gmail.com>
2022-08-11 11:26:30 +0800

    schema update, time => datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_023055) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_031948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,8 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_023055) do
     t.datetime "updated_at", null: false
     t.string "icon_url"
     t.date "start_date"
-    t.time "start_time"
-    t.time "end_time"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["user_id"], name: "index_lessons_on_user_id"
   end
 
